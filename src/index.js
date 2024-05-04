2b402a8fd750b3c9a0cf836bdd3b0f5e24f8457c
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2024-05-04 00:06:05 -0500

    Moved files, added some packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const token = process.env.DISCORD_TOKEN;
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { token } = process.env.DISCORD_TOKEN;
+const fs = require("fs");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.commands = new Collection();
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+
+for (const folder of functionFolders) {
+  const functionFiles = fs.readdirSync(`./src/functions/${folder}`);
+}
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
